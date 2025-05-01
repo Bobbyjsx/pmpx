5494d20a5c20aa42609840e545c486c27bde48cd
Godswill Ezeala <104563882+Bobbyjsx@users.noreply.github.com>
2025-05-01 17:06:57 +0100

    Fix: failing build

--- src/commands/anim.ts
+++ src/commands/anim.ts
@@ -1,13 +1,11 @@
 export async function showAnimation() {
    // Import chalk-animation as an ESM module and handle its default export
-   const chalkAnimationModule = await import("chalk-animation");
+   // const chalkAnimationModule = await import("chalk-animation");
    // Access the default export for ESM compatibility
-   const chalkAnimation = chalkAnimationModule.default || chalkAnimationModule;
-
-   const animation = chalkAnimation.rainbow("✨ pmpx: Package Manager Proxy ✨");
-
-   setTimeout(() => {
-      animation.stop();
-      console.log("\nBack to work now 😄");
-   }, 4000);
+   // const chalkAnimation = chalkAnimationModule.default || chalkAnimationModule;
+   // const animation = chalkAnimation.rainbow("✨ pmpx: Package Manager Proxy ✨");
+   // setTimeout(() => {
+   //   animation.stop();
+   //   console.log("\nBack to work now 😄");
+   // }, 4000);
 }
